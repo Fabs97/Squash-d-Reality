947b9b991a578b811ad2b025b1b719990f044fe6
Marco <marco.premi@mail.polimi.it>
2020-07-01 19:36:58 +0200

    [Fire] - Shooting mechanics implemented virst versione

---
+++ Squash'd Reality/Assets/Scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+   
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+    public float fireTime = .1f;
+    void Start()
+    {
+        StartCoroutine("ShootingRoutine");
+
+    }
+
+    void Update() {
+        // Shooting();
+        // StartCoroutine("ShootingRoutine");
+
+    }
+
+    // void Shooting(){
+    //     GameObject bullet = Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+    //     Rigidbody rb = bullet.GetComponent<Rigidbody>();
+    //     rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+    // }
+
+    IEnumerator ShootingRoutine(){
+        while(true){
+            GameObject bullet = Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+            yield return new WaitForSeconds(fireTime*Time.deltaTime);
+        } 
+    }
+}
