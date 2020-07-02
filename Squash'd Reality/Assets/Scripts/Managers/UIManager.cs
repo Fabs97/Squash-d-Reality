fc1dadb4dd885eac8bc79d3a1b57c06e7197dee6
Marco <marco.premi@mail.polimi.it>
2020-07-02 14:29:37 +0200

    [Timer] - Working timer

---
+++ Squash'd Reality/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI timerCounter;
+    public float seconds, minutes;
+    
+    
+    float timeLeft = 30.0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    { 
+        timeLeft -= Time.deltaTime; 
+        minutes = (int) (timeLeft/ 60f); 
+        seconds = (int) (timeLeft % 60f); 
+        timerCounter.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+
+    }
+
+   
+
+}
