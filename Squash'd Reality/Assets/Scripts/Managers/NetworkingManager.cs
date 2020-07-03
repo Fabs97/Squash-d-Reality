8d5f73ce848d1fbe9540f74176b6724783c1d26d
Fabs <fabrizio.siciliano@mail.polimi.it>
2020-07-03 11:40:18 +0200

    [Network] - Added functions to networking script
    Waiting for Marco's UI

--- Squash'd Reality/Assets/Scripts/Managers/NetworkingManager.cs
+++ Squash'd Reality/Assets/Scripts/Managers/NetworkingManager.cs
@@ -1,102 +1,77 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
 
 namespace NetworkingManager {
-    public class NetworkingManager : MonoBehaviour
-    {
-        private NetworkManager _networkManager;
+    public class NetworkingManager : NetworkManager {
+        private float nextRefreshTime;
+        private SceneLoader.SceneLoader _sceneLoader;
         void Awake() {
-            _networkManager = GetComponent<NetworkManager>();
+            _sceneLoader = Object.FindObjectOfType<SceneLoader.SceneLoader>();
         }
         public void createLobby(){
-            _networkManager.StartHost();
             Debug.Log("NetworkingManager::createLobby - Creating lobby");
-            Debug.Log("NetworkingManager::createLobby - networkAddress: " + _networkManager.networkAddress);
-            Debug.Log("NetworkingManager::createLobby - networkPort: " + _networkManager.networkPort);
+            base.StartHost();
+            _sceneLoader.loadNextScene("CharactersSelection");
         }
 
         public void joinLobby(){
-            _networkManager.StartClient();
-            Debug.Log("NetworkingManager::joinLobby - Joining lobby!");
-            Debug.Log("NetworkingManager::joinLobby - networkAddress: " + _networkManager.networkAddress);
-            Debug.Log("NetworkingManager::joinLobby - networkPort: " + _networkManager.networkPort);
+            Debug.Log("NetworkingManager::joinLobby - Joining lobby");
+            base.StartClient();
+        }
+
+        public void StartHosting() {
+            StartMatchMaker();
+            matchMaker.CreateMatch("default", 4, true, "", "", "", 0, 0, OnMatchCreated);
+        }
+
+        private void OnMatchCreated(bool success, string extendedinfo, MatchInfo responsedata) {
+            base.StartHost(responsedata);
+            RefreshMatches();
+        }
+
+        private void Update() {
+            if (Time.time >= nextRefreshTime) {
+                RefreshMatches();
+            }
+        }
+
+        private void RefreshMatches() {
+            nextRefreshTime = Time.time + 5f;
+
+            if (matchMaker == null)
+                StartMatchMaker();
+
+            matchMaker.ListMatches(0, 10, "", true, 0, 0, HandleListMatchesComplete);
+        }
+
+        private void HandleListMatchesComplete(bool success, 
+            string extendedinfo, 
+            List<MatchInfoSnapshot> responsedata) {
+            AvailableMatchesList.HandleNewMatchList(responsedata);
+        }
+
+        public void JoinMatch(MatchInfoSnapshot match) {
+            if (matchMaker == null)
+                StartMatchMaker();
+
+            matchMaker.JoinMatch(match.networkId, "", "", "", 0, 0, HandleJoinedMatch);
+        }
+
+        private void HandleJoinedMatch(bool success, string extendedinfo, MatchInfo responsedata) {
+            StartClient(responsedata);
         }
     }
 
-    public class HostGame : MonoBehaviour{
-        List<MatchInfoSnapshot> matchList = new List<MatchInfoSnapshot>();
-        bool matchCreated;
-        NetworkMatch networkMatch;
+	public static class AvailableMatchesList {
+		public static event System.Action<List<MatchInfoSnapshot>> OnAvailableMatchesChanged = delegate { };
 
-        void Awake() {
-            networkMatch = gameObject.AddComponent<NetworkMatch>();    
-        }
+		private static List<MatchInfoSnapshot> matches = new List<MatchInfoSnapshot>();
 
-        void OnGUI(){
-            // You would normally not join a match you created yourself but this is possible here for demonstration purposes.
-            if (GUILayout.Button("Create Room")) {
-                string matchName = "room";
-                uint matchSize = 4;
-                bool matchAdvertise = true;
-                string matchPassword = "";
-
-                networkMatch.CreateMatch(matchName, matchSize, matchAdvertise, matchPassword, "", "", 0, 0, OnMatchCreate);
-            }
-
-            if (GUILayout.Button("List rooms")) {
-                networkMatch.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
-            }
-
-            if (matchList.Count > 0) {
-                GUILayout.Label("Current rooms");
-            }
-            foreach (var match in matchList) {
-                if (GUILayout.Button(match.name)) {
-                    networkMatch.JoinMatch(match.networkId, "", "", "", 0, 0, OnMatchJoined);
-                }
-            }
-        }
-        private void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo){
-            if(success) {
-                Debug.Log("HostGame::OnMatchCreate - Create match succeeded");
-                matchCreated = true;
-                NetworkServer.Listen(matchInfo, 9000);
-                Utility.SetAccessTokenForNetwork(matchInfo.networkId, matchInfo.accessToken);
-            } else {
-                Debug.LogError("HostGame::OnMatchCreate - Create match failed: " + extendedInfo);
-            }
-        }
-
-        private void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo) {
-            if(success){
-                Debug.Log("HostGame::OnMatchJoined - Joined match successfully");
-                if(matchCreated){
-                    Debug.LogWarning("HostGame::OnMatchJoined - Match already set up, ABORTING");
-                    return;
-                }
-                Utility.SetAccessTokenForNetwork(matchInfo.networkId, matchInfo.accessToken);
-                NetworkClient myClient = new NetworkClient();
-                myClient.RegisterHandler(MsgType.Connect, OnConnected);
-                myClient.Connect(matchInfo);
-            } else {
-                Debug.LogError("HostGame::OnMatchJoined - Join match failed: " + extendedInfo);
-            }
-        }
-
-        public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches) {
-            if (success && matches != null && matches.Count > 0) {
-                networkMatch.JoinMatch(matches[0].networkId, "", "", "", 0, 0, OnMatchJoined);
-            }
-            else if (!success) {
-                Debug.LogError("List match failed: " + extendedInfo);
-            }
-        }
-
-        private void OnConnected(NetworkMessage msg){
-            Debug.Log("HostGame::OnConnected - Correctly connected: " + msg);
-        }
-    }
+		public static void HandleNewMatchList(List<MatchInfoSnapshot> matchList) {
+			matches = matchList;
+			OnAvailableMatchesChanged(matches);
+		}
+	}
 }
