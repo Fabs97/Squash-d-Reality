5a1211dd4a72194389638e1b9543335f3b71c499
Fabs <fabrizio.siciliano@mail.polimi.it>
2020-07-03 18:01:02 +0200

    [Network] - Added Spawner script, spawning player prefab

---
+++ Squash'd Reality/Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnManager : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Command]
+    public static void CmdSpawnPlayer(GameObject playerPrefab, Transform spawnPoint) {
+        GameObject go = Instantiate(playerPrefab, spawnPoint);
+        NetworkServer.Spawn(go);
+        Debug.Log("SpawnManager::CmdSpawnPlayer - Spawned my player!");
+    }
+}
