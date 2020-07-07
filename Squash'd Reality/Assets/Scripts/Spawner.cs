fefac4ba1da743df67e9cc2f4642ca2c00fc52bc
Insanitea <angelo.nasole@mail.polimi.it>
2020-07-07 17:16:15 +0200

    [ElectroPipeline] - pieces basic (ma very very basic) logic implemented

---
+++ Squash'd Reality/Assets/Scripts/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour
+{
+    public List<GameObject> toSpawn;
+    public bool isToShuffle;
+    private IEnumerator coroutine;
+    public float waitTime = 3f;
+    private int number = 0;
+
+    void Start()
+    {
+        if (isToShuffle)
+        {
+            Shuffle<GameObject>(toSpawn);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(number < toSpawn.Count)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(0, 14), 10, Random.Range(0, 14));
+            GameObject go = Instantiate(toSpawn[number], randomPos, Quaternion.identity);
+            NetworkServer.Spawn(go);
+            coroutine = WaitToSpawn(waitTime);
+            StartCoroutine(coroutine);
+        }
+        
+    }
+
+    void Shuffle<GameObject>(List<GameObject> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, list.Count);
+            GameObject value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    private IEnumerator WaitToSpawn(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        number++;
+    }
+}
