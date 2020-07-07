414d0c692c619567d08862c1125e13ac3da63b33
Insanitea <angelo.nasole@mail.polimi.it>
2020-07-07 17:36:41 +0200

    [Light] - Optimizsed script

--- Squash'd Reality/Assets/Scripts/LightOnOff.cs
+++ Squash'd Reality/Assets/Scripts/LightOnOff.cs
@@ -8,11 +8,13 @@ public class LightOnOff : MonoBehaviour
     private GameObject player;
     private Light me;
     private float luminosity;
+    private Grabber script;
 
     // Start is called before the first frame update
     void Start()
     {
         player = transform.parent.gameObject;
+        script = player.GetComponent<Grabber>();
         me = GetComponent<Light>();
         luminosity = me.intensity;
     }
@@ -20,7 +22,7 @@ public class LightOnOff : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bool on = !player.GetComponent<Grabber>().GetIsGrabbing();
+        bool on = !script.GetIsGrabbing();
 
         if (on == false) me.intensity = 0;
         else me.intensity = luminosity;
