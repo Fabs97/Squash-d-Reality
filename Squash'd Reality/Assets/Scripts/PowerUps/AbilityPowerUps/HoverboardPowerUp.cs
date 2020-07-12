df1174c5d56c4ba36ddd9458197d4e3c4b9d300d
Marco <marco.premi@mail.polimi.it>
2020-07-12 19:50:48 +0200

    [PowerUp] - All player power up implemented

---
+++ Squash'd Reality/Assets/Scripts/PowerUps/AbilityPowerUps/HoverboardPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverboardPowerUp : PowerUP
+{
+    public override void triggerEnter(Collider other)
+    {
+        UIManager uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
+        if (other.tag == "Player")
+        {
+            DummyMoveset dm =  other.gameObject.GetComponent<DummyMoveset>();
+            dm.setHoverboardActive();
+            if (dm.hasAuthority)
+            {
+                uiManager.setPowerUpImage("HoverboardPowerUp");
+                uiManager.setPowerUpName("Marty's Hoverboard");
+                uiManager.setPowerUpButtonActive(true);
+            }
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Squash'd Reality/Assets/Scripts/PowerUps/AbilityPowerUps/PogoStickPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PogoStickPowerUp : PowerUP
+{
+    public override void triggerEnter(Collider other)
+    {
+        UIManager uiManager = GameObject.FindWithTag("UIManager").GetComponent<UIManager>();
+        if (other.tag == "Player")
+        {
+            DummyMoveset dm =  other.gameObject.GetComponent<DummyMoveset>();
+            dm.setPogoStickActive();
+            if (dm.hasAuthority)
+            {
+                uiManager.setPowerUpImage("PogoStickPowerUp");
+                uiManager.setPowerUpName("Pogo stick");
+                uiManager.setPowerUpButtonActive(true);
+            }
+            Destroy(gameObject);
+
+        }
+    }
+}
