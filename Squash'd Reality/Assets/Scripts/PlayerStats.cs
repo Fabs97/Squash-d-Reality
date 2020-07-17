f14b923e5bc7b1c87b6ae248ddcf7c29bf16c4a8
Marco <marco.premi@mail.polimi.it>
2020-07-17 11:33:42 +0200

    [Leaderboard] - start commit

---
+++ Squash'd Reality/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+   //GENERAL MALUSES
+   public int death;
+   public int timeOut;
+   public int friendlyKill;
+   
+   //GENERAL BONUSES
+   public int powerUp;
+   public int collectible;
+   
+   //PIPELINE
+   public int electrocution;
+   public int cableManagement;
+   
+   //COOKING TIME
+   public int notOrdered;
+   public int greetChef;
+
+   //TRENCH TIME
+   public int trenchTimeFriendlyKill;
+   public int antivirusKilled;
+   public int professionalSniper;
+
+   private void Start()
+   {
+      resetValues();
+   }
+
+   private void resetValues()
+   {
+      death = 0;
+      timeOut = 0;
+      friendlyKill = 0;
+      powerUp = 0;
+      collectible = 0;
+      electrocution = 0;
+      cableManagement = 0;
+      notOrdered = 0;
+      greetChef = 0;
+      trenchTimeFriendlyKill = 0;
+      antivirusKilled = 0;
+      professionalSniper = 0;
+   }
+}
