804c82c9f5fbac272cdc677377335be6e4a48fbc
Insanitea <angelo.nasole@mail.polimi.it>
2020-08-06 16:05:53 +0200

    [DarkPuzzle] - Loosing a box results in Game Over

---
+++ Squash'd Reality/Assets/Scripts/DarkPuzle/DarkFall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkFall : MonoBehaviour
+{
+    private DarkPuzzle _puzzle;
+
+    void Start()
+    {
+        _puzzle = Object.FindObjectOfType<DarkPuzzle>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "DeathZone")
+        {
+            _puzzle.endChallenge(false);
+        }
+    }
+}
