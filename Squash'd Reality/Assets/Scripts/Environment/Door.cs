5c9e7fa85724cd81c860165344d99e1a418913e2
Marco <marco.premi@mail.polimi.it>
2020-08-14 17:17:37 +0200

    [Doors] - Name UI added

--- Squash'd Reality/Assets/Scripts/Environment/Door.cs
+++ Squash'd Reality/Assets/Scripts/Environment/Door.cs
@@ -1,16 +1,23 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Door : NetworkBehaviour {
     private SceneLoader.SceneLoader _sceneLoader;
     private NetworkGameManager _networkGameManager;
     
     [SerializeField] private string nextSceneName;
+    [SerializeField] private string doorName;
     [Range(1, 3)] [SerializeField] private int difficulty = 1; 
     private int playersInMe;
 
-    private void Start() {
+    private void Start()
+    {
+        TextMeshProUGUI nameDoor = transform.GetComponentInChildren<Canvas>().GetComponentInChildren<Button>()
+            .GetComponentInChildren<TextMeshProUGUI>();
+        nameDoor.text = doorName;
         _sceneLoader = Object.FindObjectOfType<SceneLoader.SceneLoader>();
         _networkGameManager = Object.FindObjectOfType<NetworkGameManager>();
     }
