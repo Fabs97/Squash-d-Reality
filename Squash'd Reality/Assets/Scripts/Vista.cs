81a77ecae7bc7f3d1586f374d6af37e502574a4f
Marco <marco.premi@mail.polimi.it>
2020-08-16 16:52:57 +0200

    [VISTA] - added in lobby

---
+++ Squash'd Reality/Assets/Scripts/Vista.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vista : MonoBehaviour
+{
+    [SerializeField] private GameObject Canvas;
+
+    private void Start()
+    {
+        Canvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            Canvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Canvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Canvas.SetActive(false);
+        }
+    }
+}
