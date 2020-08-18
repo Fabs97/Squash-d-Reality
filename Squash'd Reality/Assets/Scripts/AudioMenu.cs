f0098791d5260e28730689132db7057cffcd8067
Marco <marco.premi@mail.polimi.it>
2020-08-18 12:27:38 +0200

    [MainMenu] - audio added

---
+++ Squash'd Reality/Assets/Scripts/AudioMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AudioMenu : MonoBehaviour
+{
+    private AudioSource mainSource;
+    [SerializeField] private AudioClip[] mainMenuAudio;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        mainSource = GetComponent<AudioSource>();
+
+    }
+
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+      
+        
+    }
+    
+    void OnEnable() {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainMenu")
+        {
+            mainSource.PlayOneShot(mainMenuAudio[0]);
+
+        }
+
+        if (scene.name == "Lobby")
+        {
+            mainSource.Stop();
+        }
+    }
+}
