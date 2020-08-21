d341338736ad3f91f9c04ba73b05f0347152ae37
Marco <marco.premi@mail.polimi.it>
2020-08-21 15:35:21 +0200

    [Door] - Lobby appearance fixed

--- Squash'd Reality/Assets/Scripts/Lobby/LobbyManager.cs
+++ Squash'd Reality/Assets/Scripts/Lobby/LobbyManager.cs
@@ -11,13 +11,18 @@ public class LobbyManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       StartCoroutine(wait());
+    }
+
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(0.2f);
         _networkingManager = FindObjectOfType<NetworkingManager.NetworkingManager>();
         List<string> playedRooms = _networkingManager.getPlayedRooms();
         foreach (var door in challengeDoors)
         {
-            Debug.LogError("DOOR: " + door.GetComponent<Door>().nextSceneName);
-            Debug.LogError("ACTIVE: " + playedRooms.Contains(door.GetComponent<Door>().nextSceneName));
-            door.SetActive(playedRooms.Contains(door.GetComponent<Door>().nextSceneName));
+            door.SetActive(!playedRooms.Contains(door.GetComponent<Door>().nextSceneName));
         }
     }
 }
